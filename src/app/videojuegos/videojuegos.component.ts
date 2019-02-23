4fda051b633a3f3693492650ee95285ff59f2b25
wenguz <wenguz1518@gmail.com>
2019-02-22 20:17:54 -0500

    se crea un constructor para definir valores de las variables. En las variables se define el tipo de dato(no en el constructor)

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -5,20 +5,30 @@ import { Component } from '@angular/core';
 	templateUrl:'./videojuegos.component.html'
 })
 export class VideojuegosComponent{
-	public nombre:string ='Videojuegos 2019';
-	public mejor_juego:string ='GTA 5';
+	public nombre:string;
+	public mejor_juego:string;
 
-	public mejor_juego_retro:string = 'Super Mario 64';
-	public mostrar_retro:boolean = true;
-	public color:string ="red";
+	public mejor_juego_retro:string;
+	public mostrar_retro:boolean;
+	public color:string;
 
-	public year:number=2018;
+	public year:number;
 
-	public videojuegos:Array<string> = [
-		'Los simpson hit and run',
-		'Assins creed',
-		'GTA 5',
-		'Call of Duty',
-		'Tekken'
-	];
+	public videojuegos:Array<string>
+
+	constructor(){
+		this.nombre='Videojuegos 2019';
+		this.mejor_juego='GTA 5';
+		this.mejor_juego_retro = 'Super Mario 64';
+		this.mostrar_retor=true;
+		this.color="red";
+		this.year=2018;
+		this.videojuegos= [
+			'Los simpson hit and run',
+			'Assins creed',
+			'GTA 5',
+			'Call of Duty',
+			'Tekken'
+		];
+	}
 }
