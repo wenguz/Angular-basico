89aa934a49cf5a7234cbafe13c24a98f5d748714
wenguz <wenguz1518@gmail.com>
2019-02-22 19:22:42 -0500

    se agrego una nueva propiedad al componente

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
 	selector:'videojuegos',
 	template:`
 		<h2>Componente de {{nombre}}</h2>
+		<h3>El mejor juego es: {{mejor_juego}}</h3>
 	`
 })
 export class VideojuegosComponent{
 	public nombre='Videojuegos 2019';
+	public mejor_juego='GTA 5';
 }
