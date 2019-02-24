26a81b4a7ec90e8a5d8e7fe2dc7ad0080c93cf38
wenguz <wenguz1518@gmail.com>
2019-02-24 18:28:20 -0500

    Construccion del menu:unido admin y estandar. sin script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VideojuegosComponent } from './videojuegos/videojuegos.component';
 import { HeaderComponent } from './compartido/header/header.component';
+import { MenuComponent } from './compartido/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegosComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
