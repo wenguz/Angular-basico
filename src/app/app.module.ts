2af40cbf24a39db7fe9c953556c148a77e77c924
wenguz <wenguz1518@gmail.com>
2019-02-24 17:01:34 -0500

    Se creo la carpeta compartido y dentro el componente header: ng g c compartido/header --spec=false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VideojuegosComponent } from './videojuegos/videojuegos.component';
+import { HeaderComponent } from './compartido/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideojuegosComponent
+    VideojuegosComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
