4475a8bd4e038638eb5128a4a34a8e185d1bf5d5
wenguz <wenguz1518@gmail.com>
2019-02-24 21:38:01 -0500

    CREACION DE COMPONENTE:modificar usuario con diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MenuComponent } from './compartido/menu/menu.component';
 import { AgregarUsuarioComponent } from './admin/agregar-usuario/agregar-usuario.component';
 import { MainHeaderComponent } from './compartido/main-header/main-header.component';
 import { ListarUsuarioComponent } from './admin/listar-usuario/listar-usuario.component';
+import { ModificarUsuarioComponent } from './admin/modificar-usuario/modificar-usuario.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ListarUsuarioComponent } from './admin/listar-usuario/listar-usuario.co
     AgregarUsuarioComponent,
     MainHeaderComponent,
     ListarUsuarioComponent,
+    ModificarUsuarioComponent,
     
   ],
   imports: [
