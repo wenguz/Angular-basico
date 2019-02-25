55d78c0c9a0ef84716eb2d594310143be2d38eee
wenguz <wenguz1518@gmail.com>
2019-02-24 22:15:44 -0500

    agregaron las rutas de listar usaurio,modificar usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes,RouterModule} from '@angular/router';
 
 import {AgregarUsuarioComponent} from './admin/agregar-usuario/agregar-usuario.component';
+import {ListarUsuarioComponent} from './admin/listar-usuario/listar-usuario.component';
+import {ModificarUsuarioComponent} from './admin/modificar-usuario/modificar-usuario.component';
+
 
 const app_routes:Routes=[
 	{path:'agregar-usuario',component:AgregarUsuarioComponent},
+	{path:'listar-usuario',component:ListarUsuarioComponent},
+	{path:'modificar-usuario',component:ModificarUsuarioComponent},
 	{path:'**',pathMatch:'full', redirectTo:''}
 ];
 
